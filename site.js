4e86491069b5d39a9d579b9485f9373387073fa1
grantva <grantva@gmail.com>
2019-03-04 22:10:08 +0200

    Created site.js

---
+++ site.js
@@ -0,0 +1,11 @@
+    <script>
+      let counter = 0;
+      const btn = document.getElementById("btn");
+      const body = document.getElementsByTagName("body")[0];
+      btn.addEventListener("click", swapColors);
+      
+      function swapColors(){
+        body.setAttribute("class", counter % 2 == 0 ? "dark" : "light");
+        counter++;
+      }
+    </script>
